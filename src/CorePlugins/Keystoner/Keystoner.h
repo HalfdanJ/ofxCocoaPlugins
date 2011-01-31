9b31093ccc7f92140d5cec184dcc7ffe110d47b1
Recoil Mac Pro <jonas@halfdanj.dk>
2011-01-31 19:31:01 +0100

    Minor bug when having multiple apply surface in one scope

--- src/CorePlugins/Keystoner/Keystoner.h
+++ src/CorePlugins/Keystoner/Keystoner.h
@@ -9,9 +9,9 @@
 #define ApplySurfaceForProjector(s,p) if([Surface(s,p) visible]) { [GetPlugin(Keystoner)  applySurface:s projectorNumber:p viewNumber:ViewNumber];
 #define PopSurfaceForProjector() [GetPlugin(Keystoner)  popSurface]; }
 
-#define ApplySurface(s) int appliedProjector=-1;for(KeystoneProjector*proj in [[[GetPlugin(Keystoner) outputViews] objectAtIndex:ViewNumber] projectors]){ appliedProjector++; if(appliedProjector > 0)[GetPlugin(Keystoner)  popSurface]; ApplySurfaceForProjector(s,appliedProjector)
+#define ApplySurface(s) {int appliedProjector=-1;for(KeystoneProjector*proj in [[[GetPlugin(Keystoner) outputViews] objectAtIndex:ViewNumber] projectors]){ appliedProjector++; if(appliedProjector > 0)[GetPlugin(Keystoner)  popSurface]; ApplySurfaceForProjector(s,appliedProjector)
 
-#define PopSurface() PopSurfaceForProjector() }
+#define PopSurface() PopSurfaceForProjector() }}
 
 #include "Plugin.h"
 
