f6751c5e92563494e1a8907faf6292d8626bf4e8
Livingroom <livingroom@recoil.local>
2012-02-11 21:58:03 +0100

    OpenDMX Working

--- ofxCocoaPlugins/src/CorePlugins/OpenDMX/OpenDMX.mm
+++ ofxCocoaPlugins/src/CorePlugins/OpenDMX/OpenDMX.mm
@@ -1,30 +1,34 @@
 #import "OpenDMX.h"
 
 #import <ola/DmxBuffer.h>
-#import <ola/SimpleClient.h>
+//#import <ola/SimpleClient.h>
+#include <ola/StreamingClient.h>
+#include <ola/Logging.h>
 
 //using namespace ola;
 
-ola::SimpleClient * simpleClient;
-ola::OlaClient *client;
-ola::DmxBuffer *buffer;
+ola::StreamingClient  simpleClient;
+//ola::OlaClient *client;
+ola::DmxBuffer buffer;
 
 @implementation OpenDMX
 
 @synthesize dmxData;
 
 -(void)initPlugin{
-        
-    simpleClient = new ola::SimpleClient();
-	connected = simpleClient->Setup();
+    ola::InitLogging(ola::OLA_LOG_WARN, ola::OLA_LOG_STDERR);
+    
+    //    simpleClient = new ola::StreamingClient();
+	connected = simpleClient.Setup();
 	if (!connected) {
 		cout<<"Failed getting OLA server"<<endl;
 	}
 	
-	client = simpleClient->GetClient();
+	//client = simpleClient->GetClient();
     
-    buffer = new ola::DmxBuffer();
-    
+    //buffer = new ola::DmxBuffer();
+    buffer.Blackout();
+   
     dmxData = [NSMutableArray arrayWithCapacity:512];
     for(int i=0;i<512;i++){
         NSMutableDictionary * dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:i],@"channel", [NSNumber numberWithInt:0],@"value", nil];
@@ -38,13 +42,21 @@ ola::DmxBuffer *buffer;
     if([(NSString*)context isEqualToString:@"value"]){
         int val = [[object valueForKey:@"value"] intValue];
         int channel = [[object valueForKey:@"channel"] intValue];
-        buffer->SetChannel(channel, val);
+        //   buffer->SetChannel(channel, val);
+        [self setValue:val forChannel:channel];
     }
 }
 
 -(void) setValue:(int)val forChannel:(int)channel{
-    //buffer->SetChannel(channel, val);
-    [[dmxData objectAtIndex:channel] setObject:[NSNumber numberWithInt:val] forKey:@"value"];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+       // if(buffer.Get(channel) != val){
+//            buffer.SetChannel(channel, val)//;
+        buffer.SetRangeToValue(channel-1, val, 1);
+        //}
+       // buffer.SetRangeToValue(0, 255, 25);
+    });
+    // [[dmxData objectAtIndex:channel] setObject:[NSNumber numberWithInt:val] forKey:@"value"];
 }
 
 //
@@ -62,12 +74,18 @@ ola::DmxBuffer *buffer;
 
 -(void)update:(NSDictionary *)drawingInformation{
     unsigned int universe = 0;
-    
-    if (connected && !client->SendDmx(universe, *buffer)) {
-        
+    // buffer->SetRangeToValue(0, 100, 20);
+    if(connected){
+        dispatch_async(dispatch_get_main_queue(), ^{
+
+            if (!simpleClient.SendDmx(universe, buffer)) {
+                NSLog(@"DMX ERROR");
+            }
+          //  buffer.SetRangeToValue(0, 255, 20);
+
+        });
     }
-    
-    buffer->Blackout();
+    //buffer->Blackout();
 }
 
 //
